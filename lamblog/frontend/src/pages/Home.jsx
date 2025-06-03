907787ca9351b966dda3c01bd6c6537fd35f02e1
masxkip <jesufemiketiku@gmail.com>
2025-06-03 12:49:36 -0700

    new front update

--- lamblog/frontend/src/pages/Home.jsx
+++ lamblog/frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
   const [categories, setCategories] = useState([]);
   const [trendingPosts, setTrendingPosts] = useState([]);
 
-  // Fetch
+  // Fetch post
   const fetchPosts = useCallback(async () => {
     setLoading(true);
     try {
