e1b3d6de49d2d45af3551e72f06055db0769307b
masxkip <jesufemiketiku@gmail.com>
2025-07-08 04:24:18 -0700

    new frontt

--- lamblog/frontend/src/components/LoadingButton.jsx
+++ lamblog/frontend/src/components/LoadingButton.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { FaHourglassHalf } from "react-icons/fa";
 
-function LoadingButton({ isLoading, onClick, children, className = "", type = "button", ...props }) {
+function LoadingButton({
+  isLoading,
+  onClick,
+  children,
+  className = "",
+  type = "button",
+  ...props
+}) {
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={isLoading}
-      className={`loading-btn ${className}`}
+      className={`loading-btn ${isLoading ? "loading" : ""} ${className}`}
       {...props}
     >
       {isLoading ? (
-        <span style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            gap: "8px",
+          }}
+        >
           <FaHourglassHalf color="white" size={16} />
           <span>Processing...</span>
-        </span>
+        </div>
       ) : (
         children
       )}
