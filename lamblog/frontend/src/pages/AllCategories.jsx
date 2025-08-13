43f330a057cd9640ca3bccfdbfd392ae0ee86843
masxkip <jesufemiketiku@gmail.com>
2025-08-13 11:20:47 -0700

    new paystack backend uptdate

--- lamblog/frontend/src/pages/AllCategories.jsx
+++ lamblog/frontend/src/pages/AllCategories.jsx
@@ -10,7 +10,7 @@ import AuthContext from "../context/AuthContext";
 
 const API_URL = import.meta.env.VITE_BACKEND_URL;
 
-const CATS_PER_PAGE = 6;
+const CATS_PER_PAGE = 3;
 const LIMIT = 30; // backend page size for posts fetch
 
 function AllCategories() {
